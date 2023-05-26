31c94ed11855021c03acf4b2779060d24b2a4a4a
Dave Berner <davidajberner@gmail.com>
2023-05-26 14:55:41 +1000

    chore: remove dead files

--- pages/api/getAccessToken.js
+++
@@ -1,6 +0,0 @@
-import { getAccessToken } from "@kinde-oss/kinde-auth-nextjs";
-
-export default function handler(req, res) {
-  const token = getAccessToken(req, res);
-  res.status(200).json({ token });
-}
